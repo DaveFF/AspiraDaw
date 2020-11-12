7486abd2ddc3b7c2e546ef4b8b44444f3d39da88
David <fdezdavid144@gmail.com>
2020-11-12 13:20:06 +0100

    Base de Carga y mitad de Datos Generales

--- src/main/java/Programa/Aspiradora.java
+++ src/main/java/Programa/Aspiradora.java
@@ -1,13 +1,19 @@
 package Programa;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.JOptionPane;
 
 public class Aspiradora {
 
     public static void main(String[] args) {
-        
-        int Bateria;
+
+        int Bateria = 0;
         int SeleccionUsuario;
+
+        Date Fecha = new Date();
+
         String IPIN = JOptionPane.showInputDialog(null,
                 "Introduzca su PIN de seguridad para la AspiradoraDaw");
         int PIN = Integer.parseInt(IPIN);
@@ -18,52 +24,54 @@ public class Aspiradora {
             PIN = Integer.parseInt(FalloPIN);
 
         }
-        do {            
-            
-        
-        String Menu = JOptionPane.showInputDialog(null, "Elige opción:\n"
-                + "1.- Configuración del Sistema\n"
-                + "2.- Cargar\n"
-                + "3.- Aspirar\n"
-                + "4.- Aspirar y Fregar\n"
-                + "5.- Estado General\n"
-                + "6.- Buscar Base de Carga\n"
-                + "0.- Apagar");
-        SeleccionUsuario = Integer.parseInt(Menu);
+        do {
 
-        switch (SeleccionUsuario) {
+            String Menu = JOptionPane.showInputDialog(null, "Elige opción:\n"
+                    + "1.- Configuración del Sistema\n"
+                    + "2.- Cargar\n"
+                    + "3.- Aspirar\n"
+                    + "4.- Aspirar y Fregar\n"
+                    + "5.- Estado General\n"
+                    + "6.- Buscar Base de Carga\n"
+                    + "0.- Apagar");
+            SeleccionUsuario = Integer.parseInt(Menu);
 
-            case 1:
+            switch (SeleccionUsuario) {
 
-            case 2:
-                String BateriaO = JOptionPane.showInputDialog(null, "Has escogido cambiar la bateria\n"
-                        + "Escoga un porcentaje valido de bateria");
-                Bateria = Integer.parseInt(BateriaO);
-                if (Bateria < 1 | Bateria > 100) {
-                    BateriaO = JOptionPane.showInputDialog(null, "El valor introducido no es correcto\n"
-                            + "Recuerde que su valor tiene que ser Mayor que 0 y Menor 100");
+                case 1:
+
+                case 2:
+                    String BateriaO = JOptionPane.showInputDialog(null, "Has escogido cambiar la bateria (Valor actual " + Bateria + "%)\n"
+                            + "Escoga un porcentaje valido de bateria");
                     Bateria = Integer.parseInt(BateriaO);
-                }
-                JOptionPane.showMessageDialog(null, "El nivel de bateria se ha establecido en un " + Bateria + "%");
-                
+                    if (Bateria < 1 | Bateria > 100) {
+                        BateriaO = JOptionPane.showInputDialog(null, "El valor introducido no es correcto\n"
+                                + "Recuerde que su valor tiene que ser Mayor que 0 y Menor 100");
+                        Bateria = Integer.parseInt(BateriaO);
+                    }
+                    JOptionPane.showMessageDialog(null, "El nivel de bateria se ha establecido en un " + Bateria + "%");
 
-                break;
-            case 3:
+                    break;
+                case 3:
 
-            case 4:
+                case 4:
 
-            case 5:
+                case 5:
+                    JOptionPane.showMessageDialog(null, "Se mostrara la informacion del dispositivo");
+                    DateFormat hourdateFormat = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
+                    JOptionPane.showMessageDialog(null, "Hora y fecha: " + hourdateFormat.format(Fecha) + "\n"
+                            + "Bateria establecida: " + Bateria + "%\n");
+                    break;
+                case 6:
 
-            case 6:
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Apagando");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opcion no valida");
+            }
 
-            case 0:
-                JOptionPane.showMessageDialog(null, "Apagando");
-                break;
-            default:
-                JOptionPane.showMessageDialog(null, "Opcion no valida");
-                } 
-        
-        }while (SeleccionUsuario!=0);
+        } while (SeleccionUsuario != 0);
 
     }
 }
